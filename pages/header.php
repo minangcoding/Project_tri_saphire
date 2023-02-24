949ddc8cee92f71d8d65ba50bff682f2f2b995ea
alfad <75969052+aldyfadil@users.noreply.github.com>
2023-02-24 21:25:08 +0700

    nb

--- pages/header.php
+++ pages/header.php
@@ -9,7 +9,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
     
     <!-- css/style.css-->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="../pages/css/style.css">
     <!-- css/style.css-->
     
 </head>
