5e498e81c73c6a013cdeff95d7deb664d0011535
alfad <75969052+aldyfadil@users.noreply.github.com>
2023-02-27 13:56:39 +0700

    new

--- pages/recycle.php
+++ pages/recycle.php
@@ -11,9 +11,23 @@
     
 <?php include "header.php";?>
 
-<div">
-    <img src="img/Frame.png" class="recy">
+<div class="container-fluid">
+    <img src="img/Frame.png" class="img-fluid">
 </div>
+<br><br>
+<section class="container-fluid sectionBG sectionProdutos">
+        <article class="row">
+            <img class="col-lg-6 rounded float-start" src="img/Gro.png" id="kanan">
+            <div class="col-lg-6 cardComida">
+                <br><br><br><br><br><br><br><br>
+                <h2><span class="titulo"></span></h2>
+                <center><p><span class="descibe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus quisquam quas eius quidem tempora laborum eum animi dicta officiis blanditiis deleniti velit, quos quod perspiciatis nisi quo fugiat consequuntur.
+                <br><br>
+                <a href="#" class="btn btn-primary">SEE MORE</a></center>
+            </div>
+
+        </article>
+</section>
 
 
 
