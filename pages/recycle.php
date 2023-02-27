e3f3f64bd8bb5aff3a795f999109cbde99c910ad
alfad <75969052+aldyfadil@users.noreply.github.com>
2023-02-27 14:59:23 +0700

    change

--- pages/recycle.php
+++ pages/recycle.php
@@ -22,15 +22,53 @@
                 <br><br><br><br><br><br><br><br>
                 <h2><span class="titulo"></span></h2>
                 <center><p><span class="descibe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus quisquam quas eius quidem tempora laborum eum animi dicta officiis blanditiis deleniti velit, quos quod perspiciatis nisi quo fugiat consequuntur.
-                <br><br>
-                <a href="#" class="btn btn-primary">SEE MORE</a></center>
             </div>
 
         </article>
 </section>
+<br><br>
+<section class="container-fluid sectionBG sectionProdutos">
+        <article class="row">
+            <div class="col-lg-6 cardComida">
+                <br><br><br><br><br><br><br><br>
+                <h2><span class="titulo"></span></h2>
+                <center><p><span class="descibe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus quisquam quas eius quidem tempora laborum eum animi dicta officiis blanditiis deleniti velit, quos quod perspiciatis nisi quo fugiat consequuntur.
+            </div>
+            <img class="col-lg-6 cardrounded float-start" src="img/Grou.png" id="kanan">
 
+        </article>
+</section>
+<br><br>
+<section class="container-fluid sectionBG sectionProdutos">
+        <article class="row">
+            <img class="col-lg-6 rounded float-start" src="img/Group.png" id="kanan">
+            <div class="col-lg-6 cardComida">
+                <br><br><br><br><br><br><br><br>
+                <h2><span class="titulo"></span></h2>
+                <center><p><span class="descibe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus quisquam quas eius quidem tempora laborum eum animi dicta officiis blanditiis deleniti velit, quos quod perspiciatis nisi quo fugiat consequuntur.
+            </div>
 
+        </article>
+</section>
+<br><br>
+<section class="container-fluid sectionBG sectionProdutos">
+        <article class="row">
+            <div class="col-lg-6 cardComida">
+                <br><br><br><br><br><br><br><br>
+                <h2><span class="titulo"></span></h2>
+                <center><p><span class="descibe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum delectus quisquam quas eius quidem tempora laborum eum animi dicta officiis blanditiis deleniti velit, quos quod perspiciatis nisi quo fugiat consequuntur.
+            </div>
+            <img class="col-lg-6 cardrounded float-start" src="img/Groupp.png" id="kanan">
 
+        </article>
+</section>
+<br><br><br><br><br>
+
+<div>
+    <center><button class="btn btn-dark">LET'S TRYCICLE</button></center>
+</div>
+<br><br><br><br><br>
+<?php include "footer.php";?>
 
 
 </body>
